b1c6a45fd5d1960d668c55886fc08531cfd6f67e
Richard <richardto180@gmail.com>
2023-11-20 12:54:15 -0500

    Added function returnGrid which returns a specified number of rows and columns

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container");
 
+const input = prompt("Please enter a value for the grid");
+/*
 const row = document.createElement('div');
 row.classList.add('row');
 container.appendChild(row);
@@ -8,4 +10,19 @@ const column = document.createElement('div');
 column.classList.add('column');
 row.appendChild(column);
 column.textContent = 'Column';
+*/
+function returnGrid(row, column) {
+  for (let i = 0; i < row; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for (let j = 0; j < column; j++) {
+      const column = document.createElement('div');
+      column.classList.add('column');
+      row.appendChild(column); 
+      column.textContent = 'Column';
+    }
+  }
+}
+
 
